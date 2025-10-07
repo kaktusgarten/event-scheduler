e8c02a460894f4d50e9d1710bb5b97320267bc30
Marco Rehberg <marcorehberg@gmx.net>
2025-10-07 18:31:28 +0200

    Token local speichern - Sample auf Homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,9 +2,15 @@ import { use } from "react";
 import { GesamtseitenContext } from "../contexts/GesamtseitenContext";
 
 const HomePage = () => {
-  const { sampleText } = use(GesamtseitenContext);
-  console.log("sampleText");
-  console.log(sampleText);
+  // Wichtig für function speicherLoginToken(token):
+  const { sampleText, setLocalStorageToken, localStorageToken } =
+    use(GesamtseitenContext);
+  // Funktion zum speichern des Login Tokens:
+  function speicherLoginToken(token) {
+    console.log(`Login mit Token: ${token}`);
+    localStorage.setItem("token", JSON.stringify(token));
+    setLocalStorageToken(token);
+  }
 
   return (
     <>
@@ -12,8 +18,28 @@ const HomePage = () => {
         <h1 className="text-3xl">Events</h1>
         <p className="pb-10">Hier werden unsere Event-Einträge angezeigt:</p>
         <div className="border p-10 bg-black">
-          Ein Event Eintrag... {sampleText}
+          <p className="pb-7">Ein Event Eintrag... {sampleText}</p>
+
+          <p>Token: {localStorageToken}</p>
         </div>
+        <button
+          className="border p-5 btn m-5"
+          onClick={() => {
+            speicherLoginToken(
+              "ich bin der login token 1232134123123123 bla..."
+            );
+          }}
+        >
+          Sample Token global Speichern
+        </button>
+        <button
+          className="border p-5 btn m-5"
+          onClick={() => {
+            speicherLoginToken(null);
+          }}
+        >
+          Clear Token
+        </button>
       </div>
     </>
   );
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,9 +1,11 @@
 const LoginPage = () => {
-  return <>
-    <div className="border p-5">
-      <h1 className="text-3xl">Login-Page</h1>
-    </div>
-  </>;
+  return (
+    <>
+      <div className="border p-5">
+        <h1 className="text-3xl">Login-Page</h1>
+      </div>
+    </>
+  );
 };
 
 export default LoginPage;

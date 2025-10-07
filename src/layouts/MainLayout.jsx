ecd3bcd194beaa4a67a2d2f137fb0c67b96ba747
Marco Rehberg <marcorehberg@gmx.net>
2025-10-07 08:56:01 +0200

    Base page layout footer

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,19 +1,28 @@
 import { Outlet } from "react-router";
 import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 export default function MainLayout() {
   return (
     <>
-      <div className="bg-black">
-        <div className="container m-auto mb-4">
-          <Header></Header>
+      <div className="page flex flex-col min-h-screen">
+        <div className="bg-black">
+          <div className="container m-auto">
+            <Header></Header>
+          </div>
         </div>
-      </div>
 
-      <div className="container m-auto">
-        <main>
-          <Outlet></Outlet>
-        </main>
+        <div className="container m-auto flex-1 my-4">
+          <main>
+            <Outlet></Outlet>
+          </main>
+        </div>
+
+        <div className="bg-black">
+          <div className="container m-auto mb-4">
+            <Footer></Footer>
+          </div>
+        </div>
       </div>
     </>
   );

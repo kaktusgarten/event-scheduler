d8741e2b19b28876946a2fee166531948de64a2d
Marco Rehberg <marcorehberg@gmx.net>
2025-10-08 10:07:41 +0200

    Login Comment

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,9 +1,16 @@
 const LoginPage = () => {
-  return <>
-    <div className="border p-5">
-      <h1 className="text-3xl">Login-Page</h1>
-    </div>
-  </>;
+  return (
+    <>
+      <div className="border p-5">
+        <h1 className="text-3xl mb-4">Login-Page</h1>
+        <p className="text-xl mb-4">
+          Anmeldung...<br></br>
+          Du musst erst einen Token setzten bevor Du alle Seiten sehen kannst
+          :-)
+        </p>
+      </div>
+    </>
+  );
 };
 
 export default LoginPage;

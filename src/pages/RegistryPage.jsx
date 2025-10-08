b5fa9711c77fb4edd3fe64ab3d6ed0434bd0747d
Frank Steinmetz <frank.steinmetz@steinmetz-it.de>
2025-10-08 10:47:51 +0200

    Formulare neu angelegt

--- src/pages/RegistryPage.jsx
+++ src/pages/RegistryPage.jsx
@@ -1,74 +1,93 @@
-import { useActionState, useState } from "react";
+import { useActionState } from "react";
+
+function validateForm({ inpEmail, inpPassword }) {
+  const validationErrors = {};
+
+  if (!inpEmail.trim()) {
+    validationErrors.inpEmail = "EMail ist erforderlich";
+  }
+  if (!inpPassword.trim()) {
+    validationErrors.inpPassword = "Passwort ist erforderlich";
+  }
+  return validationErrors;
+}
+
+async function register(email, password) {
+  const response = await fetch("http://localhost:3001/api/users", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      email: email,
+      password: password,
+    }),
+  }).then();
+  if (response.ok) {
+    alert("You are successful registered");
+    console.log("Alles gut");
+  } else {
+    alert("Email always used!");
+    console.log("Status: ", response.status);
+  }
+}
+
+async function action(previousState, formData) {
+  const validateDate = Object.fromEntries(formData);
+  const formErrors = validateForm(validateDate);
+
+  const iEMail = formData.get("inpEmail");
+  const iPassword = formData.get("inpPassword");
+
+  if (Object.keys(formErrors).length === 0) {
+    // registrieren
+    register(iEMail, iPassword);
+    return { errors: null, input: null, reset: true };
+  }
+
+  return { errors: formErrors, input: null, reset: false };
+}
 
 const RegistryPage = () => {
-  const inpEMail = document.getElementById("inputEMail");
-  const inpPassword = document.getElementById("inputPassword");
-  const [formState, setFormState] = useState(false);
-  const [formErrors, setACtionState] = useActionState(action, null);
-
-  async function register(email, password) {
-    console.log("email", email);
-    console.log("password", password);
-    const response = await fetch("http://localhost:3001/api/users", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        email: email,
-        password: password,
-      }),
-    }).then();
-    if (response.ok) {
-      setFormState(false);
-      console.log("Alles gut");
-    } else {
-      setFormState(true);
-      console.log("Status: ", response.status);
-    }
-  }
+  const [state, formAction] = useActionState(action, {
+    errors: null,
+    input: null,
+    reset: true,
+  });
 
   return (
     <>
       <div className="border p-5">
         <h1 className="text-3xl">Registry-Page</h1>
+        <div className="flex flex-col mt-3 border w-3xs">
+          <form action={formAction}>
+            <input
+              type="email"
+              name="inpEmail"
+              id="inpEmail"
+              placeholder="Type your email here"
+              className="input input-md"
+            />
+            {state.errors?.inpEmail && (
+              <p className="">{state.errors.inpEmail}</p>
+            )}
 
-        <button
-          className="btn mt-5"
-          onClick={() => document.getElementById("my_modal_2").showModal()}
-        >
-          Register
-        </button>
-        <dialog id="my_modal_2" className="modal">
-          <div className="modal-box">
-            <h3 className="font-bold text-lg">Hello!</h3>
-            <p className="py-4">Press ESC key to cancel.</p>
-            <form method="dialog">
-              <input
-                type="text"
-                placeholder="Type email here"
-                className="input"
-                id="inputEMail"
-              />
-              {formState && (
-                <p className="text-red-600 mt-1 font-semibold">{"Fehler"}</p>
-              )}
+            <input
+              type="password"
+              name="inpPassword"
+              id="inpPassword"
+              placeholder="Type your password here"
+              className="input input-md"
+            />
+            {state.errors?.inpPassword && (
+              <p className="">{state.errors.inpPassword}</p>
+            )}
 
-              <input
-                type="password"
-                placeholder="Type password here"
-                className="input mt-2"
-                id="inputPassword"
-              />
-              <div className="flex justify-end">
-                <button
-                  className="btn"
-                  onClick={() => register(inpEMail.value, inpPassword.value)}
-                >
-                  Register now
-                </button>
-              </div>
-            </form>
-          </div>
-        </dialog>
+            <div className="flex mt-2 justify-end">
+              <button type="submit" className="btn btn-outline">
+                Submit
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
     </>
   );

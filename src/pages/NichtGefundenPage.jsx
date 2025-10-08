d8b0b5bcca73475f1a07e5720fdb39988cc6c0cc
Marco Rehberg <marcorehberg@gmx.net>
2025-10-08 10:19:00 +0200

    Mini 404 Page

--- src/pages/NichtGefundenPage.jsx
+++ src/pages/NichtGefundenPage.jsx
@@ -1,8 +1,22 @@
+import { Link } from "react-router";
+
 const NichtGefundenPage = () => {
   return (
     <>
-      <div className="border p-5">
-        <h1 className="text-3xl">404-Page</h1>
+      <div className="p-5 grid grid-cols-1 justify-items-center">
+        <h1 className="text-3xl mb-10">
+          404- Diese Seite wurde leider nicht gefunden
+        </h1>
+        <img
+          src="/img/404.gif"
+          alt="404 - Seite nicht gefunden"
+          className="mb-10"
+        ></img>
+        <p>
+          <Link to="/">
+            <button className="btn">Zurück zur Homepage</button>
+          </Link>
+        </p>
       </div>
     </>
   );

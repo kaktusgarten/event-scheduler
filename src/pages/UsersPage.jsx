c3cb6d720f80d06857f3ffbb1df68991c814f89e
Marco Rehberg <marcorehberg@gmx.net>
2025-10-08 16:53:53 +0200

    Benutzer list start

--- src/pages/UsersPage.jsx
+++ src/pages/UsersPage.jsx
@@ -1,9 +1,14 @@
 const UsersPage = () => {
-  return <>
-    <div className="border p-5">
-      <h1 className="text-3xl">Benutzerübersicht-Page</h1>
-    </div>
-  </>;
+  return (
+    <>
+      <div className="border p-5">
+        <h1 className="text-3xl mb-10">Benutzerübersicht</h1>
+        <ul>
+          <li>Benutzer 1 bla...</li>
+        </ul>
+      </div>
+    </>
+  );
 };
 
 export default UsersPage;

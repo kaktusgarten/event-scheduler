06b9c754f2ba27e443dd53a0e22ace1428613386
Marco Rehberg <marcorehberg@gmx.net>
2025-10-13 15:39:56 +0200

    User Table

--- src/pages/UsersPage.jsx
+++ src/pages/UsersPage.jsx
@@ -3,6 +3,10 @@ import { useEffect, useState } from "react";
 const UsersPage = () => {
   const [users, setUsers] = useState([]);
 
+  const deleteUser = (id) => {
+    alert("Lösche User: " + id);
+  };
+
   useEffect(() => {
     async function getAllUsers() {
       try {
@@ -41,9 +45,28 @@ const UsersPage = () => {
       <div className="p-5">
         <h1 className="text-3xl mb-10">Benutzerübersicht</h1>
         <ul>
-          {users?.map((user) => (
-            <li className="border p-3 m-3">
-              {user.id} - {user.email}
+          {users?.map((user, index) => (
+            <li
+              key={user.id}
+              className={`p-3 m-3 ${
+                index % 2 === 0 ? "bg-[#111c2b]" : "bg-black"
+              }`}
+            >
+              <div className="grid md:grid-cols-[80px_1fr_150px_auto] gap-4">
+                <div>{user.id}</div>
+                <div>{user.email}</div>
+                <div>Konto aktiv: {user.isActive ? "Ja" : "Nein"}</div>
+                <div className="grid justify-end">
+                  <button
+                    className="btn btn-secondary"
+                    onClick={(e) => {
+                      deleteUser(user.id);
+                    }}
+                  >
+                    löschen
+                  </button>
+                </div>
+              </div>
             </li>
           ))}
         </ul>

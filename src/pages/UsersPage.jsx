c70fc382427a849885bcb2e0b7ea21d317e4d254
Marco Rehberg <marcorehberg@gmx.net>
2025-10-16 12:20:30 +0200

    User Update Popup mit PUT Request - Noch feherhaft

--- src/pages/UsersPage.jsx
+++ src/pages/UsersPage.jsx
@@ -1,9 +1,19 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, use } from "react";
+import { GesamtseitenContext } from "../contexts/GesamtseitenContext";
 import getAllUsers from "../data/GetAllUsers";
 import deleteUser from "../data/DeleteUser";
 
 const UsersPage = () => {
   const [users, setUsers] = useState([]);
+  const [currentUserId, setCurrentUserId] = useState(null);
+  const [currentUserData, setCurrentUserData] = useState({
+    name: "",
+    email: "",
+    isActive: "",
+  });
+
+  const { localStorageToken, setLocalStorageToken } = use(GesamtseitenContext);
+  const token = "someTopSecret";
 
   useEffect(() => {
     const fetchUsers = async () => {
@@ -17,8 +27,131 @@ const UsersPage = () => {
     fetchUsers();
   }, []);
 
+  const EditUser = (id) => {
+    // filter ginge nicht, weil filter() immer ein Array zurück gibt. find() nicht.
+    const userData = users.find((user) => user.id === id);
+    console.log(userData);
+    setCurrentUserData(userData);
+    document.getElementById("my_modal_3").showModal();
+  };
+
+  const formAction = async (e) => {
+    e.preventDefault();
+
+    // 1. Formulardaten sammeln
+    const formData = new FormData(e.target);
+
+    // 2. In ein Objekt umwandeln
+    const updatedUser = {
+      name: formData.get("name"),
+      email: formData.get("email"),
+      password: formData.get("password"),
+      isActive: formData.get("active") === "true", // String → Boolean
+    };
+
+    console.log("Updated User:", updatedUser);
+
+    // // 3. PUT-Request an API
+    try {
+      const response = await fetch(
+        `http://localhost:3001/api/users/${currentUserId}`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${token}`,
+          },
+          body: JSON.stringify(updatedUser),
+        }
+      );
+
+      if (!response.ok) {
+        throw new Error(
+          `Fehler beim Aktualisieren (Status: ${response.status})`
+        );
+      }
+
+      const data = await response.json();
+      console.log("Erfolgreich aktualisiert:", data);
+
+      //   // Optional: Liste aktualisieren
+      //   setUsers((prev) =>
+      //     prev.map((user) => (user.id === currentUserId ? data : user))
+      //   );
+
+      //   // Modal schließen
+      //   document.getElementById("my_modal_3").close();
+    } catch (error) {
+      console.error("PUT Request fehlgeschlagen:", error);
+      alert(
+        "Ein Fehler ist aufgetreten, ODER: Die API ist nicht erreichbar. Für dieses Demo-Projekt muss die API lokal auf eurem Rechner installiert und ausgeführt werden :-)\nLink zur API: https://github.com/WebDev-WBSCodingSchool/events-api"
+      );
+    }
+  };
+
   return (
     <>
+      {/* MODAL */}
+      <dialog id="my_modal_3" className="modal">
+        <div className="modal-box">
+          <form onSubmit={formAction}>
+            <fieldset className="fieldset bg-base-200 border-base-300 rounded-box  border p-4">
+              <legend className="fieldset-legend">
+                Benutzer bearbeiten, ID: {currentUserId}
+              </legend>
+
+              <label className="label">Benutzername</label>
+              <input
+                defaultValue={currentUserData?.name}
+                name="name"
+                type="text"
+                className="input w-[100%] mb-4"
+                placeholder="Name eingeben"
+              />
+
+              <label className="label">Email</label>
+              <input
+                defaultValue={currentUserData?.email}
+                name="email"
+                type="email"
+                className="input w-[100%] mb-4"
+                placeholder="E-Mail eingeben"
+              />
+
+              <label className="label">Passwort</label>
+              <input
+                name="password"
+                type="text"
+                className="input w-[100%] mb-4"
+                placeholder="Password eingeben"
+              />
+              <label className="label">Kontoaktivierung</label>
+              <select
+                defaultValue={
+                  currentUserData ? String(currentUserData.isActive) : "false"
+                }
+                name="active"
+                className="select w-[100%] mb-4"
+              >
+                <option value="true" defaultChecked>
+                  aktiv
+                </option>
+                <option value="false">inaktiv</option>
+              </select>
+
+              <button type="submit" className="btn btn-neutral mt-4">
+                Speichern
+              </button>
+            </fieldset>
+          </form>
+          <form method="dialog">
+            <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+              ✕
+            </button>
+          </form>
+        </div>
+      </dialog>
+
       <div className="py-5">
         <h1 className="text-3xl mb-10 pl-3">Benutzerübersicht</h1>
         <ul>
@@ -40,6 +173,17 @@ const UsersPage = () => {
                 </div>
                 <div>Konto aktiv: {user.isActive ? "Ja" : "Nein"}</div>
                 <div className="grid justify-end">
+                  {/* BUTTON EDIT */}
+                  <button
+                    className="btn btn-primary mb-4"
+                    onClick={() => {
+                      setCurrentUserId(user.id);
+                      EditUser(user.id);
+                    }}
+                  >
+                    bearbeiten
+                  </button>
+                  {/* BUTTON DELETE */}
                   <button
                     className="btn btn-secondary"
                     onClick={async () => {
